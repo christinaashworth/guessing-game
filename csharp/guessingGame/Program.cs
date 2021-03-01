5cdd9b3ece4661877a85b5a897c58500f1785934
Christina Ashworth <christina.ashworth@ymail.com>
2021-03-01 16:03:45 -0600

    first phase finished

---
+++ csharp/guessingGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.WriteLine("Guess the secret number!");
+
+GuessNumber();
+SecretNumber();
+
+int GuessNumber()
+{
+    Console.WriteLine("What do you think it is?");
+    int answer = Console.ReadLine();
+    return answer;
+}
+
+void SecretNumber()
+{
+int secretNumber = 42;
+if secretNumber == 
+}
